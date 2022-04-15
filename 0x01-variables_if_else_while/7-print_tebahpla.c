48bfbb7a0174ce73cec45837e07baabead09beb0
Jayluv01 <waleowootomo@gmail.com>
2022-04-15 03:47:20 -0700

    8TH COMMIT

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return; Always 0
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet - 132 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
