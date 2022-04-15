4d833e9d1fac7ba0113ff03b09df4db2d2445473
Jayluv01 <waleowootomo@gmail.com>
2022-04-15 00:22:23 -0700

    First commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,18 +10,19 @@
  */
 int main(void)
 {
-int n;
+	int n;
 
-strand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n > 1)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-
-printf("%d is zero\n", n);
-
-return (0);
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{	
+		printf("%d is negative\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
 }
